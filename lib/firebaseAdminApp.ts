a681aa04210878da47afb846c1857363dfc29335
Gabriel Restori Soares <grsoares21@gmail.com>
2021-01-23 15:27:43 +0100

    Implemented user creation and autheticated scoring

---
+++ lib/firebaseAdminApp.ts
@@ -0,0 +1,23 @@
+import admin from "firebase-admin";
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    storageBucket: "np-hard-dbcf5.appspot.com",
+  });
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test(error.message)) {
+    // eslint-disable-next-line no-console
+    console.error("Firebase admin initialization error", error.stack);
+  }
+}
+
+export default admin;
--- pages/api/challenges/the-knapsack-problem/upload-solution.ts
+++ pages/api/challenges/the-knapsack-problem/upload-solution.ts
@@ -1,38 +1,75 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import runMiddleware from "../../../../lib/runMiddleware";
 import multer from "multer";
+import admin from "../../../../lib/firebaseAdminApp";
 import kanpsackInstance from "./instance_1.json";
+import firestore from "../../../../lib/firestore";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 export default async (req: any, res: any) => {
   await runMiddleware(req, res, upload.single("solution")).then(() => {});
 
-  const solution = String(req.file.buffer).split("\n");
+  const token = req.headers.authorization?.split(" ")[1];
 
-  let totalScore = 0;
-  let currentSize = 0;
+  if (token) {
+    try {
+      const { uid } = await admin.auth().verifyIdToken(token);
+      const solution = String(req.file.buffer).split("\n");
+      let newRecord = false;
 
-  for (let i = 0; i < solution.length; i++) {
-    const { value, size } = kanpsackInstance.items[+solution[i]];
+      let totalScore = 0;
+      let currentSize = 0;
 
-    currentSize += size;
-    totalScore += value;
+      for (let i = 0; i < solution.length; i++) {
+        const { value, size } = kanpsackInstance.items[+solution[i]];
 
-    if (currentSize > kanpsackInstance.knapsackSize) {
-      res.send({
-        score: 0,
-        validSolution: false,
+        currentSize += size;
+        totalScore += value;
+
+        if (currentSize > kanpsackInstance.knapsackSize) {
+          res.send({
+            score: 0,
+            validSolution: false,
+            newRecord,
+          });
+
+          return;
+        }
+      }
+
+      const scoresCollection = firestore
+        .collection("scores")
+        .doc("the-knapsack-problem")
+        .collection("scores");
+
+      const currentScore = await scoresCollection.doc(uid).get();
+
+      if (!currentScore.exists) {
+        await scoresCollection.doc(uid).set({ score: totalScore });
+        newRecord = true;
+      } else {
+        const previousScore = currentScore.data().score;
+
+        if (totalScore > previousScore) {
+          newRecord = true;
+          await scoresCollection.doc(uid).set({ score: totalScore });
+        }
+      }
+
+      return res.send({
+        score: totalScore,
+        validSolution: true,
+        newRecord,
       });
-
-      return;
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      return res.send("Unauthorized");
     }
+  } else {
+    res.status(401);
+    return res.send("Unauthorized");
   }
-
-  res.send({
-    score: totalScore,
-    validSolution: true,
-  });
 };
 
 export const config = {
