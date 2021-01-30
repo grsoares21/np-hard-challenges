22cad5891852da8bea018ad7501168f15119dbad
Murilo Maia <murilomaia.bb@gmail.com>
2021-01-30 01:29:49 -0300

    docs: add .env.example

--- lib/firebase/firebaseApp.ts
+++ lib/firebase/firebaseApp.ts
@@ -2,11 +2,11 @@ import firebase from "firebase";
 
 export default !firebase.apps.length
   ? firebase.initializeApp({
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: "np-hard-dbcf5.firebaseapp.com",
-      projectId: "np-hard-dbcf5",
-      storageBucket: "np-hard-dbcf5.appspot.com",
-      appId: "1:313327476691:web:b99c982fa180bbbbd89926",
-      measurementId: "G-YBT8S31D4Z",
-    })
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  })
   : firebase.app();
