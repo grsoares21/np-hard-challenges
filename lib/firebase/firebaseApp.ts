83d4bc37d26a27c9d7fbaa708dc1517d4ad1940f
Gabriel Restori Soares <grsoares21@gmail.com>
2021-01-30 11:32:11 +0100

    Removed outdated index page that was causing build to fail

--- lib/firebase/firebaseApp.ts
+++ lib/firebase/firebaseApp.ts
@@ -2,11 +2,11 @@ import firebase from "firebase";
 
 export default !firebase.apps.length
   ? firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-  })
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+      appId: process.env.NEXT_PUBLIC_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    })
   : firebase.app();
